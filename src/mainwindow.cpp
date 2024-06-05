3b19d8316edcf5a3108788ce8aee1c651fedac4f
Omar Ak <omarabedk@gmail.com>
2024-06-05 11:41:44 +0200

    modifying

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -15,8 +15,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     QString styleSheet = "QMainWindow { background-color: #3498db; }"
                          "QWidget#centralwidget { background-color: #ecf0f1; }"
-                         "QLabel { font-size: 15px; color: #2c3e50; }"
-                         "QPushButton { background-color: #2ecc71; color: white; border: none; border-radius: 4px; padding: 10px 20px; font-size: 16px; }"
+                         "QLabel { font-size: 15px; color: #2c3e50;text-align: center;}"
+                         "QPushButton { background-color: #2ecc71; color: white; border: none; border-radius: 4px;font-size: 16px; }"
                          "QPushButton:hover { background-color: #27ae60; }";
     this->setStyleSheet(styleSheet);
     port = new QSerialPort(this);
